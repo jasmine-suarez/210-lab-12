3ad711f855bcf0a3c78a5ece9b3b3f0b25e4487e
Jasmine <jazzyysuarez@gmail.com>
2025-09-20 08:34:42 -0700

    update

--- main.cpp
+++ main.cpp
@@ -2,10 +2,34 @@
 // IDE used: VS Code
 
 #include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <array>
+#include <fstream>
 using namespace std;
 
-int main() {
+const int DAYS = 30;
 
+int main() {
+    array<double, DAYS> gasPrices;
+
+    ifstream fin;
+    fin.open("gas_prices.txt");
+    if (fin.good()) {
+        for (int i = 0; i < DAYS; i++) {
+            fin >> gasPrices[i];
+        }
+        fin.close();
+    }
+    else
+        cout << "File not found.\n";
+
+    // testing if file is successfully accessed
+    for (int i = 0; i < DAYS; i++) {
+        cout << gasPrices[i] << ", ";
+    }
+
+    cout << "Gas prices from the last " << gasPrices.size() << ""
 
     return 0;
 }
